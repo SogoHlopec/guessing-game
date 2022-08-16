4f31e917b72caf32534659a198bc06dbf84c240f
SogoHlopec <sogo246@gmail.com>
2022-08-17 00:04:19 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
-    }
+      this.sol = Math.round((this.min + this.max) / 2);
+      return this.sol;
+  };
+    
 
     lower() {
-
+      this.max = this.sol;
     }
 
     greater() {
-
+      this.min = this.sol;
     }
 }
 
